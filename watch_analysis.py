c0cc726d06acac5e694e97b85b6fd6579c4909d6
Zack Goldblum <zackgoldblum@gmail.com>
2023-01-16 18:02:12 -0500

    Added block and trial columns to the experiment/modality DataFrame

--- watch_analysis.py
+++ watch_analysis.py
@@ -37,6 +37,7 @@ class Participant_Watch():
             filepath = os.path.join(watch_dir, modality + ".csv")
             temp_df = pd.read_csv(filepath)
             initial_ts = int(float(temp_df.columns[0]))
+
             if modality != "IBI":
                 samp_freq = int(temp_df.iloc[0][0])
                 ts_col = pd.Series([initial_ts + i/samp_freq for i in range(temp_df.size)])
@@ -44,7 +45,6 @@ class Participant_Watch():
                 temp_df = temp_df[1:]
                 temp_df.insert(loc=0, column="timestamps", value=ts_col)
                 temp_df.insert(loc=1, column="datetime", value=dt_col)
-
                 if modality == "ACC":
                     temp_df.rename(columns={temp_df.columns[2]: "accel_x", temp_df.columns[3]: "accel_y", temp_df.columns[4]: "accel_z"}, inplace=True)
                     temp_df["accel_x"] = temp_df["accel_x"]/64
@@ -65,6 +65,7 @@ class Participant_Watch():
                 temp_df = temp_df.drop(columns=temp_df.columns[2])
                 temp_df.rename(columns={temp_df.columns[2]: modality}, inplace=True)    
             df_list.append(temp_df)
+
         df = pd.concat(df_list, axis=0)
         df.reset_index(inplace=True, drop=True)
 
@@ -78,64 +79,82 @@ class Participant_Watch():
         return modality_df_dict
 
     def _create_exp_modality_dict(self):
-        def _add_block_col(exp_name, df):  
+        def _get_behav_cols(exp_name, df):  
             exp = self.par_behav.get_exp(exp_name=exp_name)
             num_blocks = exp.num_blocks
-            num_rows = df.shape[0]
-            df = df.reset_index()
-            col_list = []
+            num_rows = df.shape[0]  # num rows for the modality
+            df = df.copy()
+            df.reset_index(drop=True)
+            block_col_list = []
+            trial_col_list = []
             
             if exp_name == "audio_narrative" or exp_name == "video_narrative_cmiyc" or exp_name == "video_narrative_sherlock":
-                for ts_tuple, block in self.par_behav.by_block_ts_df[exp_name].items():
+                for ts_tuple, value_dict in self.par_behav.by_block_ts_df[exp_name].items():
                     start_ts = ts_tuple[0]
                     end_ts =  ts_tuple[1]
-                    block_start_idx = self.data_fun.get_start_index_ts(df, start_ts)
-                    block_end_idx = self.data_fun.get_end_index_ts(df, end_ts)
-                    if block_start_idx == None or block_end_idx == None:
+                    start_idx = self.data_fun.get_start_index_ts(df, start_ts)
+                    end_idx = self.data_fun.get_end_index_ts(df, end_ts)
+                    block = value_dict["block"]
+                    trial = value_dict["trial"]
+                    if start_idx == None or end_idx == None:
                         pass
                     else:
-                        col_list.append(self.data_fun.create_col(None, block_start_idx))
-                        col_list.append(self.data_fun.create_col(block, block_end_idx-block_start_idx))
-                        col_list.append(self.data_fun.create_col(None, num_rows-block_end_idx))
+                        block_col_list.append(self.data_fun.create_col(None, start_idx)) # 0 to start_idx
+                        block_col_list.append(self.data_fun.create_col(block, end_idx-start_idx))  # between start/end idx
+                        block_col_list.append(self.data_fun.create_col(None, num_rows-end_idx))  # end_idx to -1
+                        trial_col_list.append(self.data_fun.create_col(None, start_idx))
+                        trial_col_list.append(self.data_fun.create_col(trial, end_idx-start_idx))
+                        trial_col_list.append(self.data_fun.create_col(None, num_rows-end_idx))    
+
             elif exp_name == "go_no_go" or exp_name == "king_devick" or exp_name == "n_back" or exp_name == "resting_state" or exp_name == "tower_of_london" or exp_name == "vSAT":
-                for i, (ts_tuple, block) in enumerate(self.par_behav.by_block_ts_df[exp_name].items()):
+                for i, (ts_tuple, value_dict) in enumerate(self.par_behav.by_block_ts_df[exp_name].items()):
                     start_ts = ts_tuple[0]
                     end_ts =  ts_tuple[1]
-                    block_start_idx = self.data_fun.get_start_index_ts(df, start_ts)
-                    block_end_idx = self.data_fun.get_end_index_ts(df, end_ts)
-                    if block_start_idx == None or block_end_idx == None:
+                    start_idx = self.data_fun.get_start_index_ts(df, start_ts)
+                    end_idx = self.data_fun.get_end_index_ts(df, end_ts)
+                    block = value_dict["block"]
+                    trial = value_dict["trial"]
+                    if start_idx == None or end_idx == None:
                         pass
                     else:
                         if i == 0:
-                            col_list.append(self.data_fun.create_col(None, block_start_idx))
-                            col_list.append(self.data_fun.create_col(block, block_end_idx-block_start_idx))
+                            block_col_list.append(self.data_fun.create_col(None, start_idx))
+                            block_col_list.append(self.data_fun.create_col(block, end_idx-start_idx))
+                            trial_col_list.append(self.data_fun.create_col(None, start_idx))
+                            trial_col_list.append(self.data_fun.create_col(trial, end_idx-start_idx))
                         elif i == exp.num_blocks-1:
-                            col_list.append(self.data_fun.create_col(block, block_end_idx-block_start_idx))
-                            col_list.append(self.data_fun.create_col(None, num_rows-block_end_idx))
+                            block_col_list.append(self.data_fun.create_col(block, end_idx-start_idx))
+                            block_col_list.append(self.data_fun.create_col(None, num_rows-end_idx))
+                            trial_col_list.append(self.data_fun.create_col(trial, end_idx-start_idx))
+                            trial_col_list.append(self.data_fun.create_col(None, num_rows-end_idx))
                         else:
-                            col_list.append(self.data_fun.create_col(block, block_end_idx-block_start_idx))
-            try:    
-                block_col = pd.concat(col_list, axis=0, ignore_index=True)
-                df.insert(0, "block", block_col)
-            except:
-                pass
+                            block_col_list.append(self.data_fun.create_col(block, end_idx-start_idx))
+                            trial_col_list.append(self.data_fun.create_col(trial, end_idx-start_idx))
+
+            block_col = pd.concat(block_col_list, axis=0, ignore_index=True) 
+            trial_col = pd.concat(trial_col_list, axis=0, ignore_index=True)  
             
-            return df
+            return block_col, trial_col
 
         exp_modality_dict = {}
         for exp_name in self.exp_order:
-            start_dt, end_dt = self.data_fun.get_exp_dt(self.marker_ts_df, exp_name=exp_name)
+            start_dt, end_dt = self.data_fun.get_exp_dt(self.marker_ts_df, exp_name=exp_name)  # start/end of an exp
             exp_modality_data_dict = {} 
             for modality, df in self.modality_df_dict.items():
-                start_idx = self.data_fun.get_start_index_dt(df=df, start_dt=start_dt)
-                end_idx = self.data_fun.get_end_index_dt(df=df, end_dt=end_dt)
-                if start_idx == None or end_idx == None:
+                block_col, trial_col = _get_behav_cols(exp_name, df)  # get block/trial rows for the exp duration
+                start_idx = self.data_fun.get_start_index_dt(df=df, start_dt=start_dt)  # exp start idx for this modality
+                end_idx = self.data_fun.get_end_index_dt(df=df, end_dt=end_dt)  # exp end idx for this modality
+                if start_idx == None or end_idx == None: 
                     exp_modality_data_dict[modality] = None
                 else:
-                    modality_df_temp = self.modality_df_dict[modality].iloc[start_idx:end_idx]
-                    modality_df = _add_block_col(exp_name=exp_name, df=modality_df_temp)
-                    exp_modality_data_dict[modality] = modality_df
-            exp_modality_dict[exp_name] = exp_modality_data_dict
+                    block_col_sel = block_col.iloc[start_idx:end_idx].reset_index(drop=True)
+                    trial_col_sel = trial_col.iloc[start_idx:end_idx].reset_index(drop=True)
+                    modality_df = self.modality_df_dict[modality].iloc[start_idx:end_idx]  # get modality rows for the exp duration
+                    modality_df = modality_df.reset_index(drop=True)
+                    modality_df.insert(0, "trial", trial_col_sel)
+                    modality_df.insert(0, "block", block_col_sel)
+                    exp_modality_data_dict[modality] = modality_df  # key: modality, value: combined modality/behav DataFrame
+            exp_modality_dict[exp_name] = exp_modality_data_dict  # key: exp name, value: combined modality/behav dict with modalities as keys
 
         return exp_modality_dict
 
@@ -200,18 +219,21 @@ class Participant_Watch():
         self._plot_exp_regions(ax=ax)
         ax.legend(bbox_to_anchor=(1.0, 0.75), facecolor='white', framealpha=1)
 
-def create_watch_results_tables(num_pars, no_nan=False):
+def create_watch_results_tables(num_pars):
     def _create_df(par_list, exp_name, modality):
         temp_df_list = []
         for par in par_list:
             temp_df = pd.DataFrame()
             try:
+                temp_df["timestamps"] = par.exp_modality_dict[exp_name][modality]["timestamps"]
                 temp_df[modality] = par.exp_modality_dict[exp_name][modality][modality]
                 temp_df.reset_index(inplace=True, drop=True)
                 par_num_col = par.data_fun.create_col(par.par_num, num_rows=temp_df.shape[0])
                 temp_df.insert(loc=0, column="participant", value=par_num_col)
-                block_col = par.exp_modality_dict[exp_name][modality]["block"]  
-                temp_df.insert(loc=1, column="block", value=block_col)  
+                # block_col = par.exp_modality_dict[exp_name][modality]["block"]  
+                # temp_df.insert(loc=1, column="block", value=block_col)  
+                # trial_col = par.exp_modality_dict[exp_name][modality]["trial"]  
+                # temp_df.insert(loc=1, column="trial", value=trial_col)  
             except:
                 temp_df[modality] = None
             temp_df_list.append(temp_df)
@@ -219,12 +241,8 @@ def create_watch_results_tables(num_pars, no_nan=False):
         df.reset_index(inplace=True, drop=True)
         return df
 
-    def _data_to_excel(exp_name, data_dict, no_nan):
-        if no_nan:
-            filepath = os.path.join(os.getcwd(), "results", "watch_no_nan", f"{exp_name}_watch.xlsx")
-        else:
-            filepath = os.path.join(os.getcwd(), "results", "watch", f"{exp_name}_watch.xlsx")
-            
+    def _data_to_excel(exp_name, data_dict):
+        filepath = os.path.join(os.getcwd(), "results", "watch", f"{exp_name}_watch.xlsx")
         if not os.path.exists(os.path.dirname(filepath)):
             os.mkdir(os.path.dirname(filepath))
         
@@ -235,7 +253,7 @@ def create_watch_results_tables(num_pars, no_nan=False):
     par_list = []                
     for i in range(num_pars):
         par_num = f"{(i+1):02d}"
-        print("-----", par_num, "-----")
+        print("\n-----", par_num, "-----")
         par = Participant_Watch(par_num=par_num)
         par_list.append(par)
 
@@ -247,14 +265,17 @@ def create_watch_results_tables(num_pars, no_nan=False):
                 for par in par_list:
                     temp_df = pd.DataFrame()
                     try:
+                        temp_df["timestamps"] = par.exp_modality_dict[exp_name][modality]["timestamps"]
                         temp_df["accel_x"] = par.exp_modality_dict[exp_name][modality]["accel_x"]
                         temp_df["accel_y"] = par.exp_modality_dict[exp_name][modality]["accel_y"]
                         temp_df["accel_z"] = par.exp_modality_dict[exp_name][modality]["accel_z"]
                         temp_df.reset_index(inplace=True, drop=True)
                         par_num_col = par.data_fun.create_col(par.par_num, num_rows=temp_df.shape[0])
-                        temp_df.insert(0, "participant", par_num_col)
-                        block_col = par.exp_modality_dict[exp_name][modality]["block"]
-                        temp_df.insert(1, "block", block_col)
+                        temp_df.insert(loc=0, column="participant", value=par_num_col)
+                        # block_col = par.exp_modality_dict[exp_name][modality]["block"]
+                        # temp_df.insert(loc=1, column="block", value=block_col)
+                        # trial_col = par.exp_modality_dict[exp_name][modality]["trial"]  
+                        # temp_df.insert(loc=1, column="trial", value=trial_col)  
                     except:
                         temp_df["accel_x"] = None
                         temp_df["accel_y"] = None
@@ -262,15 +283,9 @@ def create_watch_results_tables(num_pars, no_nan=False):
                     temp_df_list.append(temp_df)
                 ACC_df = pd.concat(temp_df_list, axis=0)
                 ACC_df.reset_index(inplace=True, drop=True)
-                if no_nan:
-                    data_dict[modality] = ACC_df.dropna()
-                else:
-                    data_dict[modality] = ACC_df.dropna()
+                data_dict[modality] = ACC_df.dropna()
             elif modality == "BVP" or modality == "EDA" or modality == "HR" or modality == "IBI" or modality == "TEMP":
                 modality_df = _create_df(par_list, exp_name, modality)
-                if no_nan:
-                    data_dict[modality] = modality_df.dropna()
-                else:
-                    data_dict[modality] = modality_df
+                data_dict[modality] = modality_df.dropna()
 
-        _data_to_excel(exp_name, data_dict, no_nan)
+        _data_to_excel(exp_name, data_dict)
